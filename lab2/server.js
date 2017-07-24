d03935bc49556f4be986b017b16cc1c03d61d7fd
muntasir2165 <muntasir2165@hotmail.com>
2017-07-24 13:52:42 -0400

    lab2 completed with AJAX implementation

--- lab2/server.js
+++ lab2/server.js
@@ -3,6 +3,9 @@ var app = express();
 app.use(express.static('app'));
 app.use('/bower_components', express.static('bower_components'));
 // app.use('/dist', express.static('dist'));
+app.get('/welcome', function (req, res) {
+	res.send('Welcome to Module 2 Homework!');
+});
 app.listen(3000, function () {
-        console.log('Our app is listening on port 3000!');
+	console.log('Our app is listening on port 3000!');
 });
